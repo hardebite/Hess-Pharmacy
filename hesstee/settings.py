ecb9aae8926707335deb463e830e655eabb73337
hardebite <adedoyinadegbite.s@gmail.com>
2023-02-06 09:31:05 +0100

    try new database

--- hesstee/settings.py
+++ hesstee/settings.py
@@ -210,7 +210,7 @@ EMAIL_BACKEND= "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-EMAIL_HOST_USER ='adexplace@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('HOST_PASSWORD')
 
 
